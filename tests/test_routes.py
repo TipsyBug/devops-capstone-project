e85f5f1ed54a8c27e4b62487708f2d52f055a517
TipsyBug <tipsybug@gmail.com>
2023-11-24 06:53:41 -0500

    added Delete an Account function and testcases

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -160,3 +160,13 @@ class TestAccountService(TestCase):
         data = response.get_json()
         self.assertEqual(len(data), 5)
 
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        response = self.client.delete(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
